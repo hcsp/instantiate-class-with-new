9f621a46c2ffbeb545f430fffa2455a2d4d1c21f
Bo Zhang <bo@gradle.com>
2019-05-18 21:02:42 +0800

    Fix failed test

--- src/test/java/com/github/hcsp/HomeTest.java
+++ src/test/java/com/github/hcsp/HomeTest.java
@@ -5,9 +5,9 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import com.github.hcsp.pet.Cat;
 import org.junit.jupiter.api.Test;
 
-public class MainTest {
+public class HomeTest {
     @Test
     public void testNewCat() {
-        assertTrue(Main.newCat() instanceof Cat);
+        assertTrue(Home.newCat() instanceof Cat);
     }
 }
