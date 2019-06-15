18274595a041170fab87dbe5addb89e6dbbe1823
Bo Zhang <bo@gradle.com>
2019-06-15 18:43:06 +0800

    Update repo

--- src/test/java/com/github/hcsp/HomeTest.java
+++ src/test/java/com/github/hcsp/HomeTest.java
@@ -1,13 +1,12 @@
 package com.github.hcsp;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import com.github.hcsp.pet.Cat;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class HomeTest {
     @Test
     public void testNewCat() {
-        assertTrue(Home.newCat() instanceof Cat);
+        Assertions.assertTrue(Home.newCat() instanceof Cat);
     }
 }
