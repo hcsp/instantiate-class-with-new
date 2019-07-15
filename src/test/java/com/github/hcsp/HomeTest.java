53915f2fe1dafc368c1ca8effbfa9e1341415d00
Bo Zhang <zhangbodut@gmail.com>
2019-07-15 18:03:36 +0800

    Sync

--- src/test/java/com/github/hcsp/HomeTest.java
+++ src/test/java/com/github/hcsp/HomeTest.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.Test;
 
 public class HomeTest {
     @Test
-    public void testNewCat() {
-        Assertions.assertTrue(Home.newCat() instanceof Cat);
+    public void testNewCat() throws Exception {
+        Method method = Home.class.getDeclaredMethod("newCat");
+        method.setAccessible(true);
+        Assertions.assertTrue(method.invoke(null) instanceof Cat);
     }
 }
