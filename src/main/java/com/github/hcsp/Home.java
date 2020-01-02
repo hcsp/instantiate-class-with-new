8a788548d2b768110a94fbad579765c884584938
Felix Mi <41504852+mm123mm123@users.noreply.github.com>
2020-01-02 16:45:42 +0800

    Mx branch (#144)
    Mx branch (#144)

--- src/main/java/com/github/hcsp/Home.java
+++ src/main/java/com/github/hcsp/Home.java
@@ -13,6 +13,7 @@ public class Home {
 
     public static Cat newCat() {
         // Create a new Cat instance and return it here
-        // 在这里创建一个新的Cat实例并返回之
+        // 在这里创建一个新的Cat实例并返回
+        return(new Cat());
     }
 }
