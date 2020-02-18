cdf2f29999248cfcd70fa84ebc531c16ac3d4a85
zhuxueqiao <mya_dong@126.com>
2020-02-19 00:28:41 +0800

    创建一个新的对象返回

--- src/main/java/com/github/hcsp/Home.java
+++ src/main/java/com/github/hcsp/Home.java
@@ -14,5 +14,9 @@ public class Home {
     public static Cat newCat() {
         // Create a new Cat instance and return it here
         // 在这里创建一个新的Cat实例并返回之
+    /*    Cat cat = new Cat();
+       return cat;*/
+
+    return new Cat();
     }
 }
