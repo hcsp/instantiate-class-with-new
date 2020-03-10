e71d024aa85ef17e806c20503218269a0e1c0be9
7952126 <15521608275@163.com>
2020-03-10 20:18:59 +0800

    newCat222

--- src/main/java/com/github/hcsp/Home.java
+++ src/main/java/com/github/hcsp/Home.java
@@ -5,6 +5,7 @@ import com.github.hcsp.pet.Cat;
 public class Home {
     private static Cat cat;
     private static Cat newCat;
+    //我应该是在这里创建了一个新的Cat实例，这个是全局变量。
 
     public static void main(String[] args) {
         System.out.println(cat);
