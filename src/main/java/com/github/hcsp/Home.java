9bfcd0634d7141ee6245d0af081028fd59ad8cc8
何佳棋 <1149752574@qq.com>
2020-03-24 19:00:44 +0800

    Update Home.java

--- src/main/java/com/github/hcsp/Home.java
+++ src/main/java/com/github/hcsp/Home.java
@@ -14,7 +14,7 @@ public class Home {
     public static Cat newCat() {
         // Create a new Cat instance and return it here
         // 在这里创建一个新的Cat实例并返回之
-        Cat a=new Cat();
-        return a;
+         return  new Cat();
+        
     }
 }
