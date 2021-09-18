96f835988128adab03d03256084812bd151352cc
DEMAO <987854018@qq.com>
2021-09-18 17:23:19 +0800

    创建类的实例（对象）

--- src/main/java/com/github/hcsp/Home.java
+++ src/main/java/com/github/hcsp/Home.java
@@ -4,6 +4,7 @@ import com.github.hcsp.pet.Cat;
 
 public class Home {
     private static Cat cat;
+    private static Cat cat1;
 
     public static void main(String[] args) {
         System.out.println(cat);
@@ -14,5 +15,7 @@ public class Home {
     public static Cat newCat() {
         // Create a new Cat instance and return it here
         // 在这里创建一个新的Cat实例并返回之
+        Cat cat = new Cat();
+        return cat;
     }
 }
